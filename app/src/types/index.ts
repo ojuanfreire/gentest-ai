d50ded5b2392f98dd9809efdda4a9561d4905177
Pedro Henrique Barbosa Carvalho <pedrohbc1504@gmail.com>
2025-11-15 01:01:50 -0300

    Add types for User, Project and UseCase

---
+++ app/src/types/index.ts
@@ -0,0 +1,21 @@
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+export type Project = {
+  id: string;
+  name: string;
+  description: string;
+  ownerId: string;
+  createdAt: string;
+};
+
+export type UseCase = {
+  id: string;
+  title: string;
+  description: string;
+  projectId: string;
+  createdAt: string;
+};
