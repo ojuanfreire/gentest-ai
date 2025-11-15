ecdcc1bca7d50f2df19ebed3a0a3ea842442c2e3
ojuanfreire <ojuansfr9@gmail.com>
2025-11-15 17:51:26 -0300

    feat: add login logic to authService code

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import { ForgotPasswordForm } from "./features/auth/components/ForgotPasswordForm";
 import { LoginForm } from "./features/auth/components/LoginForm";
 import { RegisterForm } from "./features/auth/components/RegisterForm";
-import { ProjectArtifactsScreen } from "./features/useCases/components/ProjectArtifactsScreen";
+// import { ProjectArtifactsScreen } from "./features/useCases/components/ProjectArtifactsScreen";
 
 function App() {
   return (
