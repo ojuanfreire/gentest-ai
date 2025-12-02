bf9d3ce9e805cf21a5fe1e0d888838e17ea03033
ojuanfreire <ojuansfr9@gmail.com>
2025-12-01 21:57:08 -0300

    test: add large duplication for CI check

--- app/src/utils/mathA.ts
+++
@@ -1,5 +0,0 @@
-export const sum = (a: number, b: number) => {
-  console.log('Calculating...');
-  if (a > 0 && b > 0) return a + b;
-  return 0;
-}
--- app/src/utils/mathB.ts
+++
@@ -1,5 +0,0 @@
-export const sumOther = (x: number, y: number) => {
-  console.log('Calculating...');
-  if (x > 0 && y > 0) return x + y;
-  return 0;
-}
