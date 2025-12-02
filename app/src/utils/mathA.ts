9a75533acd36d9b65c14af813f2d986dbd7ec4f5
ojuanfreire <ojuansfr9@gmail.com>
2025-12-01 21:05:29 -0300

    test: add duplicated code

---
+++ app/src/utils/mathA.ts
@@ -0,0 +1,5 @@
+export const sum = (a: number, b: number) => {
+  console.log('Calculating...');
+  if (a > 0 && b > 0) return a + b;
+  return 0;
+}
---
+++ app/src/utils/mathB.ts
@@ -0,0 +1,5 @@
+export const sumOther = (x: number, y: number) => {
+  console.log('Calculating...');
+  if (x > 0 && y > 0) return x + y;
+  return 0;
+}
